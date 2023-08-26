30f4ac876b41ecb591f784817968df917cc052ff
muhammadehsanbhatti <ma496234@gmail.com>
2023-08-26 23:43:57 +0500

    add vechicle information

---
+++ database/migrations/2023_08_26_183105_create_rider_assets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRiderAssetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rider_assets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vechicle_id')->nullable();
+            $table->foreign('vechicle_id')->references('id')->on('rider_vechicle_information')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('vechicle_image')->nullable();
+            $table->enum('asset_type', ['Vechicle', 'License', 'Other'])->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rider_assets');
+    }
+}

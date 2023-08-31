dcbbba19cde2bdda6b8f7673c622953d1004b35c
muhammadehsanbhatti <ma496234@gmail.com>
2023-08-31 23:44:46 +0500

    add restaurant latitute

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -245,6 +245,8 @@ class RestaurantController extends Controller
             'starting_price' => 'required',
             'ordr_delivery_time' => 'required',
             'business_type' => 'required',
+            'latitude' => 'required',
+            'longitude' => 'required',
             'cuisine_id' => 'required|exists:business_cuisines,id',
         );
        
